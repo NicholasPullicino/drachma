40d4b63d1583971986ee0c0ee8fa335bd5ab3968
NicholasPullicino <56150206+NicholasPullicino@users.noreply.github.com>
2023-01-12 09:37:30 +0100

    Nicholas - Footer and header
    Added footer and search in header

--- footer.php
+++ footer.php
@@ -1,2 +1,28 @@
+<?php $footer_bg = get_theme_mod('testtheme_footer_bg', 'light'); ?>
+
+<footer class="mt-4 p-4">
+    <div class="container-fluid footer-".<?php $footer_bg ?>>
+        <div class="row">
+            <?php 
+                $footer_widget_count = get_theme_mod("testtheme_footer_widgets", "3");                   
+                for ($i = 0; $i < $footer_widget_count; $i++){
+                    echo "<div class='col'>";
+                    if (is_active_sidebar('footer_sidebar-'.($i+1))){
+                        dynamic_sidebar('footer_sidebar-'.($i+1));
+                    }
+                    echo "</div>";
+                }             
+            ?>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">   
+            <div class="col-12">
+                <p id="footerText">Created by Nicholas Pullicino</p>
+            </div>
+        </div>
+    </div>
+</footer>
 </body>
+</html>
 </html>
--- index.php
+++ index.php
@@ -1,5 +1,36 @@
-<div>
-    <h1> Hello! <h1>
-    <p> This is a test theme using Wordpress. <p>
-        <h2>Hi</h2>
+<?php
+    get_header();
+?>
+
+<div class="container text-center">
+<div class="row">
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
 </div>
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+<?php
+    get_footer();
+?>
