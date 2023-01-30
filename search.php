e0a621ded6ebc6a229e4d353540c257484e18eb5
NicholasPullicino <56150206+NicholasPullicino@users.noreply.github.com>
2023-01-30 16:40:28 +0100

    Search posts fixed

--- search.php
+++ search.php
@@ -7,7 +7,6 @@
 //creating an array and calling it from loop.php to display title when searching
 $args2 = array('section_title' => 'Search Results');
 
-// din tajjat l file tal-loop.php
     get_template_part('templateParts/loop', null, $args2);
 
 ?>
@@ -21,33 +20,13 @@ $args2 = array('section_title' => 'Search Results');
         if(have_posts()): while(have_posts( )): the_post();
         ?>
 
-            <div class="post">
-                <h2> 
-                <!-- slug in URL -->
-                <a href="<?php the_permalink(); ?>"></a>
-
-                    <?php 
-                        //fetched title stored in $post
-                        the_title()?></h2>
-
-                        <!-- date and author -->
-                        <small> <?php the_date()?> by <?php the_author_posts_link();?> </small>
-                        
-                        <!-- 3 dots -->
-                        <p> <?php the_excerpt(); ?></p>
-
-                        <!-- displays list of categories which are linked with this post  -->
-                        <p> Posted in <?php the_category(',');?> </p>
-            </div>
-
-
-
         <?php    
             endwhile;
 
             //pagination
             the_posts_pagination( array(
 
+                // mid_size kemm se juri posts per page, in this instance 2, prev_text u next_text huma in next u previous tal pagination
                 'mid_size' => 2,
                 'prev_text' => 'Prev',
                 'next_text' => 'Next'
